ba8108b19e4ac89bf40b7d563257c79303ba070a
Giacomo Bagnoli <g.bagnoli@asidev.com>
2012-07-20 14:36:29 +0200

    Removed tests, views and routes. Ignored waitress cache files

--- mokacms/__init__.py
+++ mokacms/__init__.py
@@ -5,6 +5,5 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
