d880a485153d04c9faee79e2e20e0f48f4dd6dc3
Giacomo Bagnoli <g.bagnoli@asidev.com>
2012-07-24 16:48:30 +0200

    Added upsert/drop method to model

--- mokacms/model/base.py
+++ mokacms/model/base.py
@@ -45,22 +45,52 @@ class MokaModel:
 
     def __init__(self, objinfo__=None, **kwargs):
         info = objinfo__ if objinfo__ else kwargs
-        self.__objinfo = self.schema.deserialize(info)
-        if "_id" in info:
-            self._id = info['_id']
+        object.__setattr__(self, "_objinfo", {})
+        self._objinfo = self.schema.deserialize(info)
+        self._updated = False
+        self._updated_values = set()
+        self._objid = info.get('_id', None)
 
     def __getattr__(self, attr):
         try:
-            return self.__objinfo[attr]
+            return self._objinfo[attr]
 
         except:
             raise AttributeError(attr)
 
+    def __setattr__(self, attr, value):
+        if attr in self._objinfo:
+            self._updated = True
+            self._updated_attrs.add(attr)
+            self._objinfo[attr] = value
+        else:
+            object.__setattr__(self, attr, value)
+
     def to_dict(self):
-        return self.__objinfo.copy()
+        return self._objinfo.copy()
 
     def flatten(self):
-        return self.schema.flatten(self.__objinfo)
+        return self.schema.flatten(self._objinfo)
 
     def unflatten(self, data):
-        self.__objinfo = self.schema.unflatten(data)
+        self._objinfo = self.schema.unflatten(data)
+
+    def save(self, db):
+        if self._objid:
+            self.__class__.log.debug("Updating object %s", self._objid)
+            values = {k: self._objinfo[k] for k in self._updated_attrs}
+            self.collection(db).update({"_id": self._objid}, {"$set": values})
+            self._updated = False
+            self._updated_attrs = set()
+        else:
+            self.__class__.log.debug("Creating object %s", self._objinfo)
+            self._objid = self.collection(db).insert(self._objinfo)
+
+    def delete(self, db):
+        if self._objid:
+            self.log.info("Dropping object %s", self._objid)
+            self.collection(db).drop({"_id": self._objid})
+            del self._objinfo
+            del self._id
+        else:
+            self.log.warn("Cannot drop non-created object: %s", self._objinfo)
